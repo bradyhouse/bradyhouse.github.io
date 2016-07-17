2ba2df6ba157308dbdc293b00fa56f0213527957
bradyhouse <bradyhouse@gmail.com>
2016-07-17 12:33:56 -0500

    201607010420 - closes #49, closes #48

--- angular2/fiddle-0045-AttributeDirective/app/main.ts
+++ angular2/fiddle-0045-AttributeDirective/app/main.ts
@@ -1,7 +1,9 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+
 import { AppComponent } from './app.component';
-import * as meta        from './meta';
-import * as fatalError  from './fatal-error';
+
+import * as meta from './meta';
+import * as fatalError from './fatal-error';
 
 
 console.log("%c" + meta.consoleTag, 'font-style: italic; font-size: 20px;');
